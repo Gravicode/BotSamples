28a5af5cf9ebbd4cf37555bf850a068c92fb4f6f
mifmasterz@yahoo.com <mifmasterz@yahoo.com>
2017-03-29 10:31:36 +0700

    latest compiled
    ok

---
+++ BotSample/Bots/Bot-Luis/Libs/APPCONTANTS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot_Luis
+{
+   public  class APPCONTANTS
+    {
+        //cognitive service
+        public const string MQTT_SERVER = "cloud.makestro.com";
+        public const string MQTT_USER = "mifmasterz";
+        public const string MQTT_PASS = "123qweasd";
+        public const string BING_API_KEY = "7cbea11a8b5344f08b2b5db408f91ed4";
+        public const string COMPUTERVISION_KEY = "1962967cccbe44ab943f94d5780c7b6a";
+        //public const string LUIS_APP_ID = "911822dd-93e0-4598-9b8b-b6b4882ebce9";
+        //public const string LUIS_APP_KEY = "9c5be3b5d02e4f64a744d8f929eb05ad";
+        public const string EMOTION_KEY = "cc22aa743b0340cdb9fb9094cdaadf53";
+        public const string FACE_KEY = "0b06681714024cadbd5adcd3d00a40c1";
+        public const string TEXTANALYSIS_KEY = "d850796f81484952a3fe3c6bfcaac5ba";
+        //socmed
+        public const string Twitter_ConsumerKey = "MKbCFe1z8IYVyjMJsSnvA";
+        public const string Twitter_ConsumerSecret = "zmAQFP52CTO1ZYzoFLMnLxELeyt7iYIrCVs59mFkQ";
+        public const string Twitter_AccessToken = "15628267-iFdsMnMVe6qQY6254s2PMSGFdR7a8Cf2h9kh7omnm";
+        public const string Twitter_AccessTokenSecret = "Uz9USTVa4qSh3tRjUpyvx3rCLEd1Nz6wbP0QDikZKWQ";
+
+    }
+}
